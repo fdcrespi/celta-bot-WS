6e388a385e8a50026a92c45d4f09aa7dcceac768
fede crespi <fdcrespi@gmail.com>
2025-02-17 06:32:42 -0300

    Se modifica app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const {
   createFlow,
 } = require("@bot-whatsapp/bot");
 
+require('dotenv').config();
+
 const QRPortalWeb = require("@bot-whatsapp/portal");
 const BaileysProvider = require("@bot-whatsapp/provider/baileys");
 const MySQLAdapter = require("@bot-whatsapp/database/mysql");
@@ -16,18 +18,18 @@ const { flowPrincipal } = require("./flows/flow-principal");
 /**
  * Declaramos las conexiones de MySQL
  */
-const MYSQL_DB_HOST = "192.168.0.17";
-const MYSQL_DB_USER = "crespif";
-const MYSQL_DB_PASSWORD = "C3lt4";
-const MYSQL_DB_NAME = "bot";
-const MYSQL_DB_PORT = "3306";
+const MYSQL_DB_HOST = process.env.NEXT_PUBLIC_MYSQL_HOST;
+const MYSQL_DB_USER = process.env.NEXT_PUBLIC_MYSQL_USER;
+const MYSQL_DB_PASS = process.env.NEXT_PUBLIC_MYSQL_PASS;
+const MYSQL_DB_NAME = process.env.NEXT_PUBLIC_MYSQL_DDBB;
+const MYSQL_DB_PORT = process.env.NEXT_PUBLIC_MYSQL_PORT;
 
 const main = async () => {
   const adapterDB = new MySQLAdapter({
     host: MYSQL_DB_HOST,
     user: MYSQL_DB_USER,
     database: MYSQL_DB_NAME,
-    password: MYSQL_DB_PASSWORD,
+    password: MYSQL_DB_PASS,
     port: MYSQL_DB_PORT,
   });
   const adapterFlow = createFlow([flowPrincipal]);
